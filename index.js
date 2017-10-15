d3c40d1c90e0f208e99d06c128f43d57c0a945ff
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-15 13:18:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -212,9 +212,9 @@ class CategoryGridWrapper extends Component {
     return (
       <div className="CategoryGrid" style={{backgroundColor}}>
         {title ?
-          <Row>
+          <Row>.
 //             <ComponentTitle color={titleColor} title={title} />
-            {showReadMore && categoryUrl ?
+            .{showReadMore && categoryUrl ?
               <div className="CategoryGrid-readMore">
                 <a href={categoryUrl}> <ChevronRight color="#CCC" /> </a>
               </div> : null}
